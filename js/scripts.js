21031d5113362c1c494d3a483dcf243c5775e4eb
Steve Burton and Tracie Weitzman <tracie@>
2016-11-02 10:24:53 -0700

    show random dice roll

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,24 @@
 // backend
 function Pig(player1, player2, currentScore, activePlayer) {
-  this.player1: 0;
-  this.player2: 0;
-  this.currentScore: 0;
-  this.activePlayer: 1;
+  this.player1 = 0;
+  this.player2 = 0;
+  this.currentScore = 0;
+  this.activePlayer = 1;
+}
 
-  diceRoll: function() {
-    var roll = Math.floor(Math.random() * 6) + 1;
-  }
-};
+function diceRoll() {
+  var roll = Math.floor(Math.random() * 6) + 1;
+  return roll;
+}
 
 //UI
 $(document).ready(function() {
-  $("form#player1").click(function(event) {
-    event.preventDefault();
+  $("#player1").click(function() {
     var player1DiceRollInput = $("#player1roll").val();
     var player1DiceHoldInput = $("#player1hold").val();
+
+    var newRoll = diceRoll();
+    $("#diceSingleRollTotal").text(newRoll);
 
   });
   $("form#player1").click(function(event) {
