bc2d4ee19d5b78cfc2ca63ced16b4b1545076d95
Steve Burton and Tracie Weitzman <tracie@>
2016-11-03 16:28:34 -0700

    work on js add dice roll total to player 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,75 @@
 // backend
-function Player(currentScore, totalScore) {
+function Player(currentScore, totalScore, isTurn) {
   this.currentScore = currentScore;
   this.totalScore = totalScore;
+  this.isTurn = true;
 }
-
-var player1 = new Player(0, 0);
-var player2 = new Player(0, 0);
 
 Player.prototype.diceRoll = function() {
   var roll = Math.floor(Math.random() * 6) + 1;
   if (roll === 1) {
-    player1.currentScore = 0;
-    player2.currentScore = 0
+    this.currentScore = 0;
+    this.isTurn = false;
   }
   else {
-    player1.currentScore += roll;
-    player2.currentScore += roll;
+    this.currentScore += roll;
   }
   return roll;
 }
 
-
+Player.prototype.switchActivePlayer = function() {
+  // this.currentScore = 0;
+  if (this.isTurn === true) {
+    this.isTurn = false;
+  }
+}
 
 Player.prototype.hold = function() {
-  return this.totalScore + this.currentScore;
+  //var playerTotalScore = this.totalScore + this.currentScore;
+  this.totalScore + this.currentScore;
+  this.isTurn = true;
+  //this.currentScore = 0;
 }
 
 //UI
-$(document).ready(function() {
 
+$(document).ready(function() {
+  var player1 = new Player(0, 0, "");
+  var player2 = new Player(0, 0, "");
   $("#player1roll").click(function() {
-    var player1DiceRollInput = $("#player1roll").val();
-    var player1DiceHoldInput = $("#player1hold").val();
-    var newRoll = Player.prototype.diceRoll();
+    // var player1DiceRollInput = $("#player1roll").val();
+    // var player1DiceHoldInput = $("#player1hold").val();
+    var newRoll = player1.diceRoll();
     $("#diceSingleRollTotal").text(newRoll);
     var turnRolltotal = (player1.currentScore);
     $("#diceRollTurnTotal").text(turnRolltotal);
-
-
   });
 
   $("#player1hold").click(function() {
-    // var saveScore = Player.prototype.hold(Player.totalScore);
-    // $("#player1score").text(saveScore);
+    var saveScore = player1.hold();
+    $("#player1score").text(saveScore);
+    //saveScore
     $("#diceRollTurnTotal").text("0");
     $("#diceSingleRollTotal").text("");
     $("#player1Buttons").hide();
     $("#player2Buttons").show();
-
   });
+
+  $("#player2roll").click(function() {
+    var newRoll = player2.diceRoll();
+    $("#diceSingleRollTotal").text(newRoll);
+    var turnRolltotal = (player2.currentScore);
+    $("#diceRollTurnTotal").text(turnRolltotal);
+  });
+
+  $("#player2hold").click(function() {
+    var saveScore = player2.hold();
+    $("#player2score").text(saveScore);
+    // player2.switchActivePlayer();
+    $("#diceRollTurnTotal").text("0");
+    $("#diceSingleRollTotal").text("");
+    $("#player2Buttons").hide();
+    $("#player1Buttons").show();
+  });
+
 });
